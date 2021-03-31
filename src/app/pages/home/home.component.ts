31dbc2c8b8953fd0f190d0bd556a1bf66cb9a5c8
JohnErikcObando <Jeobando11@hotmail.com>
2021-03-30 20:41:57 -0500

    Monstrar video en una ventana Modal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutobeService } from '../../services/youtobe.service';
-import { YouTobeReponse } from '../../models/youTobe.models';
+import { YouTobeReponse, Video } from '../../models/youTobe.models';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,41 @@ import { YouTobeReponse } from '../../models/youTobe.models';
 })
 export class HomeComponent implements OnInit {
 
+  videos: Video[]=[];
+
   constructor( private youTobeService: YoutobeService) { }
 
   ngOnInit(): void {
 
     this.youTobeService.getVideos()
       .subscribe((resp) => {
-        console.log(resp);
+        // console.log(resp);
+        this.videos.push( ... resp);
+        console.log(this.videos);
+        
     });
+  }
+
+  mostrarVideo(video:Video){
+    console.log(video);
+    Swal.fire({
+      html:`
+      <h4> ${video.title}</h4>
+      <iframe width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; 
+              autoplay;
+               clipboard-write; 
+              encrypted-media; gyroscope; 
+              picture-in-picture" 
+              allowfullscreen>
+      </iframe>
+      `
+    })
+    
   }
 
 }
